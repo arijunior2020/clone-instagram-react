8b52616f7fc5d742ddbf9f8c0cd43e4f468a387f
Arimatéia Júnior <arimateiajunior.tic@gmail.com>
2024-12-05 20:10:50 -0300

    raw: Concluindo projeto

--- src/components/Suggestions/Suggestion.jsx
+++ src/components/Suggestions/Suggestion.jsx
@@ -1,4 +1,10 @@
+import React, { useState } from "react";
+
 function Suggestion({ img, user, reason }) {
+    const [seguindo, setSeguindo] = useState(false);
+
+    const toogleSeguindo = () => setSeguindo(!seguindo);
+    
     return (
       <div className="sugestao">
         <div className="usuario">
@@ -8,7 +14,7 @@ function Suggestion({ img, user, reason }) {
             <div className="razao">{reason}</div>
           </div>
         </div>
-        <div className="seguir">Seguir</div>
+        <div className={`seguir} ${seguindo ? "seguindo" : ""}`} onClick={toogleSeguindo} style={{cursor: "pointer", color: seguindo ? "#555": "#139EF2"}}>{seguindo ? "seguindo" : "seguir"}</div>
       </div>
     );
   }
